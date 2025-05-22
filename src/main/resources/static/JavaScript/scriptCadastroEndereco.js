0f473f82ad0f66431d9034116d956c10fd13d571
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-05-22 12:47:04 -0300

    Update scriptCadastroEndereco.js

--- src/main/resources/static/JavaScript/scriptCadastroEndereco.js
+++ src/main/resources/static/JavaScript/scriptCadastroEndereco.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 nomeRua,
                 numeroCasa,
                 cidade,
-				bairro,
+		bairro,
                 estado,
                 cep,
                 comp,
